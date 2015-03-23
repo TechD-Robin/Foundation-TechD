21b3c7a83038231ccbb9a8ac7027f16fbe9a8066
Robin Hsu <robinhsu599+dev@gmail.com>
2015-03-24 00:01:44 +0800

    Initial Commit

---
+++ Foundation+TechD/Foundation_TechD.h
@@ -0,0 +1,13 @@
+//
+//  Foundation_TechD.h
+//  Foundation+TechD
+//
+//  Created by Robin Hsu on 2015/3/24.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Foundation_TechD : NSObject
+
+@end
---
+++ Foundation+TechD/Foundation_TechD.m
@@ -0,0 +1,13 @@
+//
+//  Foundation_TechD.m
+//  Foundation+TechD
+//
+//  Created by Robin Hsu on 2015/3/24.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import "Foundation_TechD.h"
+
+@implementation Foundation_TechD
+
+@end
