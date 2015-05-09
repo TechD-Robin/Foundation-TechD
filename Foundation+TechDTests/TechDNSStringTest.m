43d7f92f8b6634580284561368b7c116e5d5a774
Robin Hsu <robinhsu599+dev@gmail.com>
2015-05-09 14:22:28 +0800

    add a string method : compareByRegularExpression & add the uniit test for this method.

---
+++ Foundation+TechDTests/TechDNSStringTest.m
@@ -0,0 +1,51 @@
+//
+//  TechDNSStringTest.m
+//  Foundation+TechD
+//
+//  Created by Robin Hsu on 2015/5/9.
+//  Copyright (c) 2015年 TechD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "Foundation+TechD.h"
+
+@interface TechDNSStringTest : XCTestCase
+
+@end
+
+@implementation TechDNSStringTest
+
+//  ------------------------------------------------------------------------------------------------
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+//  ------------------------------------------------------------------------------------------------
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+//  ------------------------------------------------------------------------------------------------
+- ( void ) testRegularExpression
+{
+    NSString                  * testString;
+    
+    testString                  = @"abc";
+    NSLog( @"regular expression parse result : %d", [testString compareByRegularExpression: @"(\\w)+"] );
+    
+    testString                  = @"ab c";
+    NSLog( @"regular expression parse result : %d", [testString compareByRegularExpression: @"(\\w)+"] );
+    
+    
+}
+
+//  ------------------------------------------------------------------------------------------------
+//  ------------------------------------------------------------------------------------------------
+
+@end
+
+
+
